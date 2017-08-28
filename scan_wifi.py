06d875a2333891fd8938d70ded587fab17b72553
Nick Donald <nick@farmshelf.com>
2017-08-28 17:54:28 -0400

    update wifi scan

--- scan_wifi.py
+++ scan_wifi.py
@@ -9,7 +9,7 @@ def scan():
 
     process = sp.Popen(['iw', config.hostapd.iface, 'scan', 'ap-force'], stdout=sp.PIPE)
     stdout, _ = process.communicate()
-    for line in stdout.split('\n'):
+    for line in stdout.decode('UTF-8').split('\n'):
         ssid_mt = re.search(ssid_re, line)
         signal_mt = re.search(signal_re, line)
         if ssid_mt:
