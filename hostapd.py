3440d1acf973b0402cd06fc2fdec050de74e47f7
Nick Donald <nick@farmshelf.com>
2018-03-14 14:42:38 -0400

    Try to restart hostapd if it fails + ssid name customize

--- hostapd.py
+++ hostapd.py
@@ -1,16 +1,22 @@
 import config
 import random
 import time
+import os
 import subprocess as sp
 
 CONF_FILE = '/tmp/hostapd.conf'
 
 process = None
 
+MAX_ATTEMPTS = 5
+attempts = 0
+
 def start():
     global process
+    global attempts
     if config.hostapd.ssid_randomize:
-        ssid = config.hostapd.ssid + '_' + str(random.randint(1, 1000))
+        addon = os.environ['RESIN_DEVICE_NAME_AT_INIT'] if 'RESIN_DEVICE_NAME_AT_INIT' in os.environ else str(random.randint(1, 1000))
+        ssid = config.hostapd.ssid + '_' + addon
     else:
         ssid = config.hostapd.ssid
 
@@ -27,6 +33,14 @@ wmm_enabled=0
         f.write(conf_file)
 
     process = sp.Popen(['hostapd', CONF_FILE], stdout=sp.PIPE, stderr=sp.PIPE)
+    time.sleep(2)
+    process.poll()
+    if process.returncode:
+        if attempts <= MAX_ATTEMPTS:
+            restart()
+            attempts += 1
+        else:
+            print("HOSTAPD: max attempts reached")
 
 def stop():
     process.kill()
